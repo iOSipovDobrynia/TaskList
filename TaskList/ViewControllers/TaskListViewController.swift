81c50f8d09a9123659eecad462cc95c0261806f5
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-09-23 18:59:55 +0300

    add editing task

--- TaskList/ViewControllers/TaskListViewController.swift
+++ TaskList/ViewControllers/TaskListViewController.swift
@@ -49,7 +49,7 @@ final class TaskListViewController: UITableViewController {
     
     @objc
     private func addNewTask() {
-        showAlert(with: "New task", and: "input your task name")
+        showAlert(withTitle: "New task", andMessage: "input your task name")
     }
     
     private func fetchData() {
@@ -81,14 +81,27 @@ extension TaskListViewController {
     }
 }
 
+// MARK: - UITableViewDelegate
+extension TaskListViewController {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedTask = taskList[indexPath.row]
+        showAlert(withTitle: "Task", andMessage: "Change your task name", oldTask: selectedTask, at: indexPath)
+    }
+}
+
 // MARK: - UIAlertController
 extension TaskListViewController {
-    private func showAlert(with title: String, and message: String) {
+    private func showAlert(withTitle title: String, andMessage message: String, oldTask: Task? = nil, at indexPath: IndexPath? = nil) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         
         let saveAction = UIAlertAction(title: "Save", style: .default) { [weak self] action in
-            guard let task = alert.textFields?.first?.text, !task.isEmpty else { return }
-            self?.save(task)
+            guard let taskName = alert.textFields?.first?.text, !taskName.isEmpty else { return }
+            
+            if let _ = oldTask, let indexPath = indexPath {
+                self?.editOldTaskName(to: taskName, at: indexPath)
+            } else {
+                self?.save(taskName)
+            }
         }
         
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
@@ -98,6 +111,9 @@ extension TaskListViewController {
         
         alert.addTextField { textField in
             textField.placeholder = "New task"
+            
+            guard let task = oldTask else { return }
+            textField.text = task.title
         }
         
         present(alert, animated: true)
@@ -107,6 +123,21 @@ extension TaskListViewController {
         let task = Task(context: viewContext)
         task.title = taskName
         
+        saveContext()
+        taskList.append(task)
+        let cellIndex = IndexPath(row: taskList.count - 1, section: 0)
+        tableView.insertRows(at: [cellIndex], with: .automatic)
+    }
+    
+    private func editOldTaskName(to newTaskName: String, at indexPath: IndexPath) {
+        let task = taskList[indexPath.row]
+        task.title = newTaskName
+        
+        saveContext()
+        tableView.reloadRows(at: [indexPath], with: .automatic)
+    }
+    
+    private func saveContext() {
         if viewContext.hasChanges {
             do {
                 try viewContext.save()
@@ -114,9 +145,5 @@ extension TaskListViewController {
                 print(error.localizedDescription)
             }
         }
-        
-        taskList.append(task)
-        let cellIndex = IndexPath(row: taskList.count - 1, section: 0)
-        tableView.insertRows(at: [cellIndex], with: .automatic)
     }
 }
