2c308cfafb1a66375e2a182f25ddb7f5ec7159e7
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-09-26 23:01:09 +0300

    alertController

--- TaskList/ViewControllers/TaskListViewController.swift
+++ TaskList/ViewControllers/TaskListViewController.swift
@@ -99,7 +99,6 @@ extension TaskListViewController {
             storageManager.delete(taskList[indexPath.row])
             taskList.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .automatic)
-            storageManager.saveContext()
         }
     }
 }
@@ -107,28 +106,32 @@ extension TaskListViewController {
 // MARK: - UITableViewDelegate
 extension TaskListViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let selectedTask = taskList[indexPath.row]
-        showAlert(withTitle: "Task", andMessage: "Change your task name", oldTask: selectedTask, at: indexPath)
         tableView.deselectRow(at: indexPath, animated: true)
+        let selectedTask = taskList[indexPath.row]
+        showAlert(withTitle: "Task", andMessage: "Change your task name", task: selectedTask) {
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        }
     }
 }
 
 // MARK: - UIAlertController
 extension TaskListViewController {
-    private func showAlert(withTitle title: String, andMessage message: String, oldTask: Task? = nil, at indexPath: IndexPath? = nil) {
+    private func showAlert(withTitle title: String, andMessage message: String, task: Task? = nil, completion: (() -> Void)? = nil) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         
         let saveAction = UIAlertAction(title: "Save", style: .default) { [weak self] action in
             guard let taskName = alert.textFields?.first?.text, !taskName.isEmpty else { return }
             
-            if let _ = oldTask, let indexPath = indexPath {
-                self?.update(to: taskName, at: indexPath)
-            } else {
+            guard let task = task else {
                 self?.create(taskName)
+                return
+            }
+            if let completion = completion {
+                completion()
             }
         }
         
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
         
         alert.addAction(saveAction)
         alert.addAction(cancelAction)
@@ -136,7 +139,7 @@ extension TaskListViewController {
         alert.addTextField { textField in
             textField.placeholder = "New task"
             
-            guard let task = oldTask else { return }
+            guard let task = task else { return }
             textField.text = task.title
         }
         
